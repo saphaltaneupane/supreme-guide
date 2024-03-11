1a93488c56eaeedb1bfe08ed9cf6f2e1309e2713
saphaltaneupane <neupanesaphalta@gmail.com>
2024-03-11 18:28:45 +0545

    java

--- src/main/java/com/bway/springproject/controller/UserController.java
+++ src/main/java/com/bway/springproject/controller/UserController.java
@@ -2,6 +2,7 @@ package com.bway.springproject.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,11 +19,14 @@ public class UserController {
 	 return "LoginForm";
  }
 	@PostMapping("/login")
-	public String postLogin(@ModelAttribute  User user) {
+	public String postLogin(@ModelAttribute  User user,Model model) {
 		User usr = userService.userLogin(user.getEmail(), user.getPassword());
 		if(usr!=null) {
+			model.addAttribute("uname",usr.getFname());
+			
 			return "Home";
 		}
+		model.addAttribute("error","user not found");
 		return "LoginForm";
 	}
 	@GetMapping("/signup")
@@ -32,6 +36,10 @@ public class UserController {
 	@PostMapping("/signup")
 	public String postSignup(@ModelAttribute User user) {
 		userService.userSignup(user);
+		return "LoginForm";
+	}
+	@GetMapping("/logout")
+	public String logout() {
 		return "LoginForm";
 	}
 }
