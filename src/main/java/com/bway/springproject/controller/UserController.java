25d26a5fff6ea2341d5fc5f2ed086eb8730fa64d
saphaltaneupane <neupanesaphalta@gmail.com>
2024-03-18 06:49:50 +0545

    jav

--- src/main/java/com/bway/springproject/controller/UserController.java
+++ src/main/java/com/bway/springproject/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.bway.springproject.controller;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,9 +9,11 @@ import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bway.springproject.model.User;
 import com.bway.springproject.service.UserService;
+import com.bway.springproject.utils.VerifyRecaptcha;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.java.Log;
@@ -23,7 +27,9 @@ public class UserController {
 	 return "LoginForm";
  }
 	@PostMapping("/login")
-	public String postLogin(@ModelAttribute  User user,Model model ,HttpSession session) {// global variable for right user to access data
+	public String postLogin(@ModelAttribute  User user,Model model ,HttpSession session,@RequestParam("g-recaptcha-response") String gcapCode) throws IOException {// global variable for right user to access data
+		if(VerifyRecaptcha.verify(gcapCode)) {
+	
 		user.setPassword(DigestUtils.md5DigestAsHex(user.getPassword().getBytes()));
 		User usr = userService.userLogin(user.getEmail(), user.getPassword());
 		if(usr!=null) {
@@ -34,9 +40,14 @@ public class UserController {
 			//model.addAttribute("uname",usr.getFname());
 			
 			return "Home";
+		}else {
+			log.info("-------- login failed---------");
+			model.addAttribute("error","user not found");
+			return "LoginForm";
+		}
 		}
 		log.info("-------- login failed---------");
-		model.addAttribute("error","user not found");
+		model.addAttribute("error","are you robort?");
 		return "LoginForm";
 	}
 	@GetMapping("/signup")
