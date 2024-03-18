d4c3c64613a4aa4f1a88e2b6261f199fe5a392e5
saphaltaneupane <neupanesaphalta@gmail.com>
2024-03-18 19:20:16 +0545

    java

--- src/main/java/com/bway/springproject/controller/EmployeeController.java
+++ src/main/java/com/bway/springproject/controller/EmployeeController.java
@@ -6,10 +6,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.bway.springproject.model.Employee;
 import com.bway.springproject.service.DepartmentService;
 import com.bway.springproject.service.EmployeeService;
+import com.bway.springproject.utils.EmployeeExcelView;
+import com.bway.springproject.utils.EmployeePdfView;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -33,4 +37,45 @@ public class EmployeeController {
 		empService.addEmployee(emp);
 		 return "redirect:/employee";
 	 }
+	@GetMapping("/employeeList")
+	public String getDepartments(Model model) {
+		model.addAttribute("eList",empService.getAllEmployees());
+		return"EmployeeListForm";
+	}
+	@GetMapping("/emp/delete") // deleting the data
+	public String delete(@RequestParam long id ) {
+		empService.deleteEmployee(id);
+		return "redirect:/employeeList";
+	}
+	@GetMapping("/emp/edit")
+	public String edit(@RequestParam long id, Model model) {
+		
+		model.addAttribute("emodel",empService.getEmployeeById(id));
+		return "EmployeeListEdit";
+	}
+	@PostMapping("/emp/update")
+	public String update(@ModelAttribute Employee emp) {
+		empService.updateEmployee(emp);
+		return "redirect:/employeeList";
+	}
+	//for exporting excels and pdfs
+	
+		@GetMapping("/emp/excel")
+		public ModelAndView excel() {
+			
+			ModelAndView mv  = new ModelAndView();
+			mv.addObject("eList",empService.getAllEmployees());
+			mv.setView(new EmployeeExcelView());
+			
+			return mv;
+			
+		}
+		
+		@GetMapping("/emp/pdf")
+		public ModelAndView pdf() {
+			ModelAndView mv  = new ModelAndView();
+			mv.addObject("eList",empService.getAllEmployees());
+			mv.setView(new EmployeePdfView());
+			return mv;
+		}
 }
---
+++ src/main/java/com/bway/springproject/utils/EmployeeExcelView.java
@@ -0,0 +1,70 @@
+package com.bway.springproject.utils;
+import com.bway.springproject.model.Employee;
+
+import java.util.List;
+
+import java.util.Map;
+
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.web.servlet.view.document.AbstractXlsxView;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class EmployeeExcelView extends AbstractXlsxView {
+
+	@Override
+	protected void buildExcelDocument(
+			Map<String, Object> model, 
+			Workbook workbook, 
+			HttpServletRequest request,
+			HttpServletResponse response) 
+					throws Exception {
+
+		//1. define your own excel file name
+		response.addHeader("Content-Disposition", "attachment;filename=employee.xls");
+		
+		//2. read data given by Controller
+		@SuppressWarnings("unchecked")
+		List<Employee> list = (List<Employee>) model.get("eList");
+		
+		//3. create one sheet
+		Sheet sheet = workbook.createSheet("EMPLOYEE");
+		
+		//4. create row#0 as header
+		setHead(sheet);
+		
+		//5. create row#1 onwards from List<T> 
+		setBody(sheet,list);
+	}
+
+	private void setHead(Sheet sheet) {
+		Row row = sheet.createRow(0);
+		row.createCell(0).setCellValue("ID");
+		row.createCell(1).setCellValue("EMPLOYEE FIRST NAME");
+		row.createCell(2).setCellValue("EMPLOYEE LAST NAME");
+		row.createCell(3).setCellValue("EMPLOYEE GENDER");
+//		row.createCell(4).setCellValue("EMPLOYEE ADDRESS");
+		row.createCell(4).setCellValue("EMPLOYEE PHONE");
+		row.createCell(5).setCellValue("EMPLOYEE EMAIL");
+	}
+	
+	private void setBody(Sheet sheet, List<Employee> list) {
+		int rowNum = 1;
+		for(Employee spec : list) {
+			Row row = sheet.createRow(rowNum++);
+			row.createCell(0).setCellValue(spec.getId());
+			row.createCell(1).setCellValue(spec.getFname());
+//			row.createCell(2).setCellValue(spec.getLname());
+//			row.createCell(3).setCellValue(spec.getGender());
+////			row.createCell(4).setCellValue(spec.getAddress());
+//			row.createCell(4).setCellValue(spec.getPhone());
+//			row.createCell(5).setCellValue(spec.getEmail());
+		}
+	}
+
+}
